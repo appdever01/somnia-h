048e31e27b50b4fdece068e500ec7bfe07342fdc
appdever01 <appdever01@gmail.com>
2025-09-19 09:27:22 +0100

    Enhance ESLint configuration by disabling the 'react/no-unescaped-entities' rule and adjusting linter options for unused disable directives

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,10 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off",
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: "off",
     },
   },
 ];
